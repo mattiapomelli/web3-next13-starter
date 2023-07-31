4d6a213b014286478a46ef78730ceece431aebe2
mattiapomelli <mattiapomelli@gmail.com>
2023-07-31 12:07:09 +0200

    Show wallet status only if not reconnecting

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useAccount } from "wagmi";
+
 import { Logo } from "@/components/logo";
 import { ThemeToggle } from "@/components/ui/theme-toggle";
 
@@ -6,14 +10,18 @@ import { WalletStatus } from "../wallet/wallet-status";
 import { Container } from "./container";
 
 export const Navbar = () => {
+  const { isConnecting, isReconnecting } = useAccount();
+
   return (
     <header className="flex h-20 items-center">
       <Container className="flex w-full items-center justify-between">
         <Logo />
-        <div className="flex items-center gap-4">
-          <ThemeToggle />
-          <WalletStatus />
-        </div>
+        {!isReconnecting && !isConnecting && (
+          <div className="flex items-center gap-4 duration-100 animate-in fade-in">
+            <ThemeToggle />
+            <WalletStatus />
+          </div>
+        )}
       </Container>
     </header>
   );
