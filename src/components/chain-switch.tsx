e5d2c5c29f2a9fa0e4a8a730111aef9f24b96409
mattiapomelli <mattiapomelli@gmail.com>
2023-10-29 19:44:56 +0100

    Use web3modal for chain switch

--- src/components/chain-switch.tsx
+++ src/components/chain-switch.tsx
@@ -1,8 +1,7 @@
-import { ChevronDownIcon } from "@heroicons/react/24/solid";
-import { Chain, useNetwork, useSwitchNetwork } from "wagmi";
+import { useWeb3Modal } from "@web3modal/wagmi/react";
+import { Chain, useNetwork } from "wagmi";
 
 import { Button } from "@/components/ui/button";
-import { Dropdown, DropdownContent, DropdownItem, DropdownTrigger } from "@/components/ui/dropdown";
 import { CHAIN_ICON } from "@/constants/chains";
 
 interface ChainIconProps {
@@ -16,36 +15,21 @@ const ChainIcon = ({ chain, className }: ChainIconProps) => {
 };
 
 export const ChainSwitch = () => {
-  const { chain, chains } = useNetwork();
-  const { switchNetwork } = useSwitchNetwork();
+  const { chain } = useNetwork();
+  const { open } = useWeb3Modal();
 
   return (
-    <Dropdown className="inline-flex">
-      <DropdownTrigger>
-        {!chain || chain.unsupported ? (
-          <Button size="sm" color="error">
-            Unsupported network
-          </Button>
-        ) : (
-          <span className="rounded-btn flex items-center gap-3 bg-base-200 px-4 py-1.5 font-medium hover:bg-base-300">
-            <ChainIcon chain={chain} className="h-6 w-6" />
-            <span className="hidden sm:block">{chain?.name}</span>
-            <ChevronDownIcon className="h-4 w-4" />
-          </span>
-        )}
-      </DropdownTrigger>
-      <DropdownContent className="mt-3">
-        {chains.map((chain) => (
-          <DropdownItem
-            key={chain.id}
-            onClick={() => switchNetwork?.(chain.id)}
-            className="flex items-center gap-3"
-          >
-            <ChainIcon chain={chain} className="h-5 w-5" />
-            {chain.name}
-          </DropdownItem>
-        ))}
-      </DropdownContent>
-    </Dropdown>
+    <button onClick={() => open({ view: "Networks" })}>
+      {!chain || chain.unsupported ? (
+        <Button size="sm" color="error">
+          Unsupported network
+        </Button>
+      ) : (
+        <span className="rounded-btn flex items-center gap-3 bg-base-200 px-4 py-1.5 font-medium hover:bg-base-300">
+          <ChainIcon chain={chain} className="h-6 w-6" />
+          <span className="hidden sm:block">{chain?.name}</span>
+        </span>
+      )}
+    </button>
   );
 };
